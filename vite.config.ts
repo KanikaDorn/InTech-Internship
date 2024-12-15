369df6a3992e3a9f2e4792eb22e703c88f8519f3
KanikaDorn <kanika.dorn22@kit.edu.kh>
2024-12-16 01:01:17 +0700

    Fix issues with build process and update3

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    outDir: 'build'  // Change this to 'build' if you want the output to go into the 'build' folder
+  }
+});
+
