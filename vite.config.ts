9160ad7c15820d8e2f53c9f55b380f2b9c2b5ed9
KanikaDorn <kanika.dorn22@kit.edu.kh>
2024-12-16 01:08:34 +0700

    Fix issues with build process and update4

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-
 export default defineConfig({
   build: {
-    outDir: 'build'  // Change this to 'build' if you want the output to go into the 'build' folder
-  }
-});
+    rollupOptions: {
+      // https://rollupjs.org/configuration-options/
+    },
+  },
+})
 
